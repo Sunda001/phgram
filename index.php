b6eb0f128b9ae6e27fa55b54438a271ae2ce73d1
Yusuf_M_Thon_iD <32301831+Sunda001@users.noreply.github.com>
2019-08-26 09:42:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require 'phgram.phar';
-$bot = new Bot('904830657:AAGNiizYDYmA9qylmJfxG3QVjZJWe3bqrHc');
+$bot = new Bot('904830657:AAF56gfJU6lNxbTTxl-M3CeI7UjmR3oPWqs');
 
 $text = $bot->Text();
 $chat_id = $bot->ChatID();
